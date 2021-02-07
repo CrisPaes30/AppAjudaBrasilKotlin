23eb8ac0d0b3320cc3a6575230b0d87f15a655b3
745725Ab <cristiapaes90@gmail.com>
2021-02-07 11:43:23 -0300

    Finalizando App

--- app/src/main/java/paes/cristian/com/br/MainActivity.kt
+++ app/src/main/java/paes/cristian/com/br/MainActivity.kt
@@ -6,33 +6,65 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    var list: MutableList<Proposicao> = emptyList<Proposicao>().toMutableList()
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: ProposicoesAdapter
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        requestQueue = Volley.newRequestQueue(this)
+
+        recyclerView = findViewById(R.id.rv_propositicoes)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adapter = ProposicoesAdapter(list, this, requestQueue)
+        recyclerView.adapter = adapter
+
+        load()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun load() {
+
+        val request = JsonObjectRequest(Request.Method.GET,
+            "https://dadosabertos.camara.leg.br/api/v2/proposicoes?ordem=ASC&ordenarPor=id",
+            null,
+            Response.Listener<JSONObject> {
+
+                list.clear()
+                val dados = it.getJSONArray("dados")
+                for (i in 0 until dados.length()) {
+                    val item = dados.getJSONObject(i)
+                    list.add(
+                        Proposicao(
+                            item.getInt("id"),
+                            item.getString("uri"),
+                            item.getString("siglaTipo"),
+                            item.getInt("ano"),
+                            item.getString("ementa")
+                        )
+                    )
+                }
+
+                adapter.notifyDataSetChanged()
+
+            },
+            null)
+
+        requestQueue.add(request)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
